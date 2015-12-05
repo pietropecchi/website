3bfaa436111f370018d4d2087d973ef1d7909c10
pietropecchi <pietro.pecchi@hotmail.it>
2015-12-05 01:38:42 +0100

    about page + footer update

--- scripts.js
+++ scripts.js
@@ -10,19 +10,20 @@ $(document).on('scroll', function () {
               $('nav').css('line-height', '80px');
 
               $('section').css('top', '80px');
+                $('footer').css('top', '80px');
     }, function(){
     $('nav').css("opacity", 1);
               $('nav').css('line-height', '80px');
 
               $('section').css('top', '80px');
-    
+         $('footer').css('top', '80px');
 });
     
  $('nav').css('opacity', 1);
           $('nav').css('line-height', '80px');
      
             $('section').css('top', '80px');   
-    
+         $('footer').css('top', '80px');
 } else {
     
     
@@ -31,15 +32,17 @@ $(document).on('scroll', function () {
     $('nav').css('opacity', 0.85);
            $('nav').css('line-height', '80px');
             $('section').css('top', '0px');
+             $('footer').css('top', '0px');
     }, function(){
     $('nav').css("opacity", 0.4);
            $('nav').css('line-height', '80px');
             $('section').css('top', '0px');
-    
+         $('footer').css('top', '0px');
 });
 $('nav').css("opacity", 0.4);
               $('nav').css('line-height', '80px');   
               $('section').css('top', '0px');
+                   $('footer').css('top', '0px');
 }
 
 });
